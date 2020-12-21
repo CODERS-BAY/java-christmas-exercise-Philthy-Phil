36b1e8667a6015e49eac2c0f84c395a206879194
Philthy-Phil <phil.ziegelbauer@gmx.at>
2020-12-21 21:13:46 +0100

    setup-StringManipulations

---
+++ task3/StringManipulations.java
@@ -0,0 +1,11 @@
+public class StringManipulations {
+
+    public static void main(String[] args) {
+
+        String test = "012345";
+
+        System.out.println("hello world");
+        System.out.println(test.substring(2, test.length()));
+    }
+
+}
